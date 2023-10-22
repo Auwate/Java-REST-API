0839a42e1ee1c39896f0d7e36ba63264e289fc20
Auwate <auwate1@gmail.com>
2023-10-22 14:46:40 -0400

    - Create updates branch - Create new class - Student

--- SpringBootTraining/src/main/java/com/example/SpringBootTraining/SpringBootTrainingApplication.java
+++ SpringBootTraining/src/main/java/com/example/SpringBootTraining/SpringBootTrainingApplication.java
@@ -1,10 +1,13 @@
 package com.example.SpringBootTraining;
 
+import com.example.SpringBootTraining.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,10 +20,15 @@ public class SpringBootTrainingApplication {
 	}
 
 	@GetMapping
-	public List<String > hello () {
-		List<String> jsonValues = new ArrayList<>();
-		jsonValues.add("Hello");
-		jsonValues.add("World");
+	public List<Student> hello () {
+		List<Student> jsonValues = new ArrayList<>();
+		jsonValues.add(new Student(
+				1L,
+				"Austin",
+				"austin.w.uwate@gmail.com",
+				LocalDate.of(2003, Month.FEBRUARY, 23),
+				20
+		));
 		return jsonValues;
 	}
 
